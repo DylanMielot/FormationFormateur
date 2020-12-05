27f85ae30baa43255e0b05e8a435f2d7f03428db
anthony.melin <anthony.melin@laposte.net>
2020-12-05 17:38:46 +0100

    Maj exo formulaire

--- Exercices/Exercice_7_Formulaire.php
+++ Exercices/Exercice_7_Formulaire.php
@@ -2,11 +2,35 @@
 <html>
     <head>
         <title> TP - Base de données</title>
+        <link rel="stylesheet" href="style/css/main.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <meta charset="utf-8" />
     </head>
-    <body>
+    <body style="background-image: url('../style/images/wallpaper.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;">
+    <div class="container" style="margin-top:5%">
+    <div class="card" style="background-color: rgba(256, 256, 256, 0.95);">
+    <h3 class="card-header" style="text-align:center;">Exercice formulaire</h3>
+    <div class="card-body" style="text-align:center;">
 
-    Hello world
+
+    <!--====================================== ZONE D'EXERCICE =====================================
     
-    </body>
+    Ajoutez les element html necessaire afin de recupérer le nom, prenom et l'age de la personne a saisir
+    
+    -->
+
+
+        <form action="formulaire.php" method="post">
+
+            <input type='text' name='Chaine' value='Azerty'>
+            <input type='submit' name='Bouton'>
+
+        </form>
+
+
+    <!--============================================================================================-->
+
+    </div></div></div></body>
 </html>
---
+++ Exercices/formulaire.php
@@ -0,0 +1,37 @@
+<?php
+
+echo "Donnees recues :";
+foreach($_POST as $key=>$value) {
+	echo "<ul>".$key." => ".$value."</ul>";
+}
+
+//====================================== ZONE D'EXERCICE =====================================//
+
+	// A partir des données recues, implementez les requetes necessaires afin :
+	//	- d'ajouter une personne
+	//  - de supprimer une personne
+	
+
+	// 1 - Recupereration des donnees de la variable $_POST
+	// exemple : $valeur_bouton = $_POST['Bouton'];
+
+
+
+
+
+	// 2 - Connexion à la base de donnees -> Utilisation de PDO
+
+
+
+
+
+	// 3 - Utilisation de PDO pour effectuer la requete
+
+
+
+
+
+//============================================================================================//
+
+?>
+<a href='Exercice_7_Formulaire.php'> Retour a la page </a>
