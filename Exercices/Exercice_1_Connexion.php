d7a04b62eda15ab243e1f0633d8b6243b70dc475
anthony.melin <anthony.melin@laposte.net>
2020-12-05 17:46:10 +0100

    Correction exo 1

--- Exercices/Exercice_1_Connexion.php
+++ Exercices/Exercice_1_Connexion.php
@@ -7,12 +7,12 @@ function connect()
 
 
 
-    $sql_hostname=""; //Nom de l'hote
-    $sql_dbname=""; //Nom de la base de données a laquelle vous souhaitez vous connecter
-    $sql_user=""; //Nom de l'utilisateur pour se connecter a la base de données
+    $sql_hostname="127.0.0.1"; //Nom de l'hote
+    $sql_dbname="FormaFor"; //Nom de la base de données a laquelle vous souhaitez vous connecter
+    $sql_user="root"; //Nom de l'utilisateur pour se connecter a la base de données
     $sql_mdp=""; //Mot de passe de l'utilisateur 
 
-    return new PDO('mysql:');
+    return new PDO('mysql:host='.$sql_hostname.';dbname='.$sql_dbname.';=charset=utf8', $sql_user, $sql_mdp);
 
 
 //===========================================================================================//
